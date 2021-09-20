56e662cd2b5db90078d5fdcc0e4e756ddd2d0b75
gift134 <32878522+gift134@users.noreply.github.com>
2021-09-20 09:07:21 +0800

    Add files via upload
    new

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once "Careerjet_API.php" ;
+
+$locale = $_GET["locale"];
+
+$keywords = $_GET["keywords"];
+
+$location = $_GET["location"];
+
+$page = $_GET["page"];
+
+$affid = $_GET["affid"];
+
+
+
+
+function jobs_search(){
+	$api = new Careerjet_API($locale) ;
+
+$result = $api->search(array(
+  'keywords' => $keywords,
+  'location' => $location,
+  'page' => $page,
+  'affid' => $affid,
+));
+
+error_reporting(E_ALL);
+
+ini_set("display_errors", 1);
+
+header('Content-Type: application/json; charset=utf-8');
+
+return json_encode($result, JSON_PRETTY_PRINT);
+
+}
+
+?>
